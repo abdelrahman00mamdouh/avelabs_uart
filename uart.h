fe774cd7f5e2e1183c0b05af74581362336e59c9
Abdelrahman Mamdouh <abdelrahmanmamdouh66@gmail.com>
2022-09-21 21:15:21 +0200

    Create uart.h

---
+++ uart.h
@@ -0,0 +1,10 @@
+ "############# uart.h ##############"
+   
+#ifndef_UART_H_
+#def_UART_H_
+#define RXEN 4
+#define TXEN 3
+#define UCSRC_Config 0b10000110
+#define baudrate 9600
+#define set_bit(var,bit_no) var|=(1<<bit_no)
+#endif
